f237b33924a346b302d2b863b08c013462020415
null15 <30800470+null15@users.noreply.github.com>
2025-12-17 21:41:58 +0100

    Change GamePackageName to 'com.blade.bd'

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.dts.freefireth"
+#define GamePackageName "com.blade.bd"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
